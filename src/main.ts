83f5646fb981e63c49de11d4afbebe6a220ec000
Luiz Carlos <rosestolato@paytime.com.br>
2023-04-03 22:21:02 -0300

    fix: add limit bodyparse

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('/api');
   app.getHttpAdapter().getInstance().disable('x-powered-by');
+  app.use(bodyParser.json({ limit: '35mb' }));
+  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
   app.enableCors({ origin: '*' });
   await app.listen(process.env.PORT);
 }
